87b6f4b01d67b4a009a629c20dad68d4d1f66291
Luis Alberto <wiz_Alberto@hotmail.com>
2018-09-06 21:36:37 -0600

    Commit inicial... ?

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require ('express');
+const hbs = require ('hbs');
+const fs = require ('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials/')
+app.set('viwe engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);    
+    fs.appendFile('server.log', log + '\n', (err) =>{
+        if (err) {
+            console.log('Unable to append to server .log!');
+        }
+    })
+    next();
+});
+/* app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+}); */
+app.use(express.static(__dirname + '/public/'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res)=>{
+    res.render('home.hbs',{
+        pageTitle: 'Diz is di jom peich dog!',
+        todayDate: 'el día de hoy'
+    });
+});
+
+app.get ('/about', (req,res)=>{
+    res.render('about.hbs',{
+        pageTitle: 'Avau Peich'
+    });
+});
+app.get ('/bad', (req,res)=>{
+    res.send({
+        nombrePagina: 'Bad',
+        mensaje: 'Bad Conection',
+        resultados:{
+            status: '400',
+            conexion:'error'
+        }
+    });
+})
+
+app.listen(3000, () => {
+    console.log('ATENCION: El servidor listo en el puerto 3000 :)');
+    
+});
