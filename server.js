a737a2222c6bc54b763e8678f70dff1c9debcf57
Luis Alberto <wiz_Alberto@hotmail.com>
2018-09-10 22:57:17 -0600

    Despues de heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const hbs = require ('hbs');
 const fs = require ('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -56,7 +57,7 @@ app.get ('/bad', (req,res)=>{
     });
 })
 
-app.listen(3000, () => {
-    console.log('ATENCION: El servidor listo en el puerto 3000 :)');
+app.listen(port, () => {
+    console.log(`ATENCION: El servidor listo en el puerto ${port} :)`);
     
 });
